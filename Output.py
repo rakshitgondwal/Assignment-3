26342e451775f2ab23ebb75273876f4ac27b2dc3
Rakshit Gondwal <98955085+rakshitgondwal@users.noreply.github.com>
2022-06-08 21:58:16 +0530

    Create Output.py

---
+++ Output.py
@@ -0,0 +1,102 @@
+*** Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32. ***
+*** Remote Python engine is active ***
+>>> 
+*** Remote Interpreter Reinitialized ***
+Enter Number:3
+0b11
+>>> 
+
+
+
+
+
+*** Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32. ***
+*** Remote Python engine is active ***
+>>> 
+*** Remote Interpreter Reinitialized ***
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 3
+Enter first number: 2
+Enter second number: 3
+2.0 * 3.0 = 6.0
+Let's do next calculation? (yes/no): no
+>>> 
+
+
+
+
+
+
+*** Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32. ***
+*** Remote Python engine is active ***
+>>> 
+*** Remote Interpreter Reinitialized ***
+35
+Enter Value of n:3
+3.0
+Enter value of r:2
+50.24
+Eter value of a:1
+Enter value of b:4
+1.3150487433024949
+Eter value of y2:6
+Enter value of y1:7
+Eter value of x1:8
+Enter value of x2:9
+1.0
+>>> 
+
+
+
+
+
+
+*** Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32. ***
+*** Remote Python engine is active ***
+>>> 
+*** Remote Interpreter Reinitialized ***
+a part
+0
+1
+2
+3
+4
+b part
+3
+4
+5
+6
+7
+8
+9
+c part
+4
+7
+10
+d part
+15
+13
+11
+9
+7
+e part
+>>> 
+
+
+
+
+
+
+*** Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32. ***
+*** Remote Python engine is active ***
+>>> 
+*** Remote Interpreter Reinitialized ***
+Enter Number of Hydrogen atoms2
+Enter Number of Carbon atoms3
+Enter Number of Oxygen atoms4
+The total combined molecular weight in grams per mole is 102
+>>> 
